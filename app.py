276e6ac928a45df3c9eac1ca9a49aa4c49242bf1
tumbleshack <bmckaym@gmail.com>
2023-06-08 12:44:44 -0400

    Boilerplate login, logout, register, mfa

---
+++ app.py
@@ -0,0 +1,48 @@
+import os
+
+from flask import Flask, render_template_string
+from flask_security import Security, current_user, auth_required, hash_password, \
+     SQLAlchemySessionUserDatastore
+from database import init_db, db_session
+from models import User, Role
+from passlib.totp import generate_secret
+
+# Create app
+app = Flask(__name__, instance_relative_config=True)
+
+# Load the default configuration
+app.config.from_object('config.default')
+
+# Load the configuration from the instance folder
+app.config.from_pyfile('config.py')
+
+# Load the file specified by the APP_CONFIG_FILE environment variable
+# Variables defined here will override those in the default configuration
+env_config_module = os.environ.get('APP_CONFIG_FILE', 'config.development')
+app.config.from_object(env_config_module)
+
+# Setup Flask-Security
+user_datastore = SQLAlchemySessionUserDatastore(db_session, User, Role)
+app.security = Security(app, user_datastore)
+
+# Views
+@app.route("/")
+@auth_required()
+def home():
+    return render_template_string('Hello {{email}} !', email=current_user.email)
+
+# one time setup
+with app.app_context():
+    # Create a user to test with
+    init_db()
+    if not app.security.datastore.find_user(email="test@me.com"):
+        app.security.datastore.create_user(email="test@me.com", password=hash_password("password"))
+    db_session.commit()
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db_session.remove()
+
+if __name__ == '__main__':
+    # run application (can also use flask run)
+    app.run()
---
+++ database.py
@@ -0,0 +1,30 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from urllib.parse import urlparse
+import importlib
+import pathlib
+import os
+
+env_config_module = os.environ.get('APP_CONFIG_FILE', 'config.development')
+app_config = importlib.import_module('config.default')
+env_config = importlib.import_module(env_config_module)
+app_config.__dict__.update(env_config.__dict__)
+
+db_url = urlparse(app_config.SQLALCHEMY_DATABASE_URI)
+path = pathlib.Path(db_url.path[1:])
+path.parent.mkdir(parents=True, exist_ok=True)
+
+engine = create_engine(app_config.SQLALCHEMY_DATABASE_URI)
+Base = declarative_base()
+db_session = scoped_session(sessionmaker(autocommit=False,
+                                            autoflush=False,
+                                            bind=engine))
+Base.query = db_session.query_property()
+
+def init_db():
+    # import all modules here that might define models so that
+    # they will be registered properly on the metadata.  Otherwise
+    # you will have to import them first before calling init_db()
+    import models
+    Base.metadata.create_all(bind=engine)

2d94ebf2b9079d1fbbe7800e39fed41bd5420ab1
tumbleshack <bmckaym@gmail.com>
2023-06-09 17:55:24 -0400

    Adding meet & fixing redundant relationship

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,16 +1,23 @@
 from sqlalchemy.orm import relationship, Mapped
-from .swim import Team, UserTeamRole
+from .swim import Team, UserTeamRole, Meet, team_meet_association
 from .user import User, Role
-from typing import Set
+from typing import Set, List
 
 def configure_mappers():
     UserTeamRole.user = relationship(User)
     UserTeamRole.team = relationship(Team)
     UserTeamRole.role = relationship(Role)
 
-    User.roles: Mapped[Set["UserTeamRole"]] = relationship(UserTeamRole, back_populates="user")
+    User.roles: Mapped[Set["Role"]] = relationship(UserTeamRole, back_populates="user", viewonly=True)
+    Team.users: Mapped[Set["User"]] = relationship(UserTeamRole, back_populates="team", viewonly=True)
+    User.teams: Mapped[Set["Team"]] = relationship(UserTeamRole, back_populates="user", viewonly=True)
 
-    Team.users: Mapped[Set["UserTeamRole"]] = relationship(UserTeamRole, back_populates="team")
-    User.teams: Mapped[Set["UserTeamRole"]] = relationship(UserTeamRole, back_populates="user")
+    Meet.guest_teams: Mapped[List[Team]] = relationship(
+        Team, secondary=team_meet_association, back_populates="guest_meets"
+    )
+    Team.guest_meets: Mapped[List[Meet]] = relationship(
+        Meet, secondary=team_meet_association, back_populates="guest_teams"
+    )
+    Team.host_meets: Mapped[List[Meet]] = relationship(Meet, back_populates="host_team")
 
 configure_mappers()
--- backend/models/swim.py
+++ backend/models/swim.py
@@ -1,7 +1,15 @@
 from .utils import TimestampMixin
 from database import Base
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import Integer, String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import Integer, String, ForeignKey, Column, Table
+from typing import Optional
+
+team_meet_association = Table(
+    "team_meet_table",
+    Base.metadata,
+    Column("team_id", ForeignKey("team_table.id"), primary_key=True),
+    Column("meet_id", ForeignKey("meet_table.id"), primary_key=True),
+)
 
 class Team(Base, TimestampMixin):
     __tablename__ = 'team_table'
@@ -15,3 +23,11 @@ class UserTeamRole(Base, TimestampMixin):
     user_id: Mapped[int] = mapped_column(ForeignKey("user_table.id"), unique=True)
     team_id: Mapped[int] = mapped_column(ForeignKey("team_table.id"), unique=True)
     role_id: Mapped[int] = mapped_column(ForeignKey("role_table.id"), unique=True)
+
+class Meet(Base, TimestampMixin):
+    __tablename__ = 'meet_table'
+    id = mapped_column(Integer, primary_key=True)
+    name = mapped_column(String(255), unique=True, nullable=False)
+    description = mapped_column(String(255), nullable=True)
+    host_team_id: Mapped[Optional[int]] = mapped_column(ForeignKey("team_table.id"))
+    host_team: Mapped[Optional[Team]] = relationship(back_populates="host_meets")

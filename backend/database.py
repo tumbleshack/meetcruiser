d12a69928b1090751fbbb53c0dfe68502bbad804
tumbleshack <bmckaym@gmail.com>
2023-06-13 22:45:22 -0400

    Autocreate marshmallow schemas for alchemy models

--- backend/database.py
+++ backend/database.py
@@ -1,5 +1,6 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
+from marshmallow_sqlalchemy import ModelConversionError, SQLAlchemyAutoSchema
+from sqlalchemy import create_engine, event
+from sqlalchemy.orm import scoped_session, sessionmaker, mapper
 from sqlalchemy.ext.declarative import declarative_base
 from urllib.parse import urlparse
 import importlib
@@ -21,6 +22,34 @@ db_session = scoped_session(sessionmaker(autocommit=False,
                                             autoflush=False,
                                             bind=engine))
 Base.query = db_session.query_property()
+
+
+def setup_schema(Base, session):
+    # Create a function which incorporates the Base and session information
+    def setup_schema_fn():
+        for class_ in Base.registry._class_registry.values():
+            if hasattr(class_, "__tablename__"):
+                if class_.__name__.endswith("Schema"):
+                    raise ModelConversionError(
+                        "For safety, setup_schema can not be used when a"
+                        "Model class ends with 'Schema'"
+                    )
+
+                class Meta(object):
+                    model = class_
+                    sqla_session = session
+
+                schema_class_name = "%sSchema" % class_.__name__
+
+                schema_class = type(
+                    schema_class_name, (SQLAlchemyAutoSchema,), {"Meta": Meta}
+                )
+
+                setattr(class_, "__marshmallow__", schema_class)
+
+    return setup_schema_fn
+
+event.listen(mapper, "after_configured", setup_schema(Base, db_session))
 
 def init_db():
     # import all modules here that might define models so that
